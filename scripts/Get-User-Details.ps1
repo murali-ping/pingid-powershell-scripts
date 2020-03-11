6c8dd5635d06f013f2f10326a4848c4648df1142
murali-ping <62049434+murali-ping@users.noreply.github.com>
2020-03-11 12:09:40 +0530

    Update Get-User-Details.ps1

--- scripts/Get-User-Details.ps1
+++ scripts/Get-User-Details.ps1
@@ -7,8 +7,6 @@ PingID API Sample Powershell Script: Get-User-Details.ps1
 
 This script will query the PingID API GetUserDetails operation to return the details about a PingID user. For more information, review the API documentation: https://developer.pingidentity.com/en/api/pingid-api.html
 
-Note: This software is open sourced by Ping Identity but not supported commercially as such. Any questions/issues should go to the Github issues tracker or discuss on the Ping Identity developer communities. See also the DISCLAIMER file in this directory.
-
 .PARAMETER UserName
 
 The PingID user name you want to retrieve details about.
